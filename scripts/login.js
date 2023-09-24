784d11e91c49f5aede8548a44f34df8e7f148836
Jaime Meza <jaimealbertomeza@hotmail.com>
2023-09-23 19:11:38 -0500

    More fields and validations added

--- scripts/login.js
+++ scripts/login.js
@@ -1,23 +1,17 @@
 import { ValidateUser } from './users.js';
 
-// Get references to the button and input elements
 const loginButton = document.getElementById('loginButton');
 const emailInput = document.getElementById('form2Example17');
 const passwordInput = document.getElementById('form2Example27');
 
-// Add a click event listener to the button
 loginButton.addEventListener('click', () => {
-  // Get the values of the email and password inputs
   const emailValue = emailInput.value;
   const passwordValue = passwordInput.value;
 
-  // Perform credential validation here (replace with your logic)
   if (ValidateUser(emailValue, passwordValue)) {
-    // Credentials are correct, perform the desired action (e.g., redirect to a new page)
-    alert('Login successful!'); // Replace with your action
+    alert('Login exitoso!');
     window.location.href = "../pages/users-table.html";
   } else {
-    // Credentials are incorrect, display an error message or take appropriate action
-    alert('Invalid email or password'); // Replace with your action
+    alert('Email o contraseña invalidos');
   }
 });
--- scripts/users.js
+++ scripts/users.js
@@ -1,17 +1,20 @@
 // users.js
 
 class User {
-  constructor(name, lastName, email, password) {
+  constructor(name, lastName, identification, address, phone, birthdate, email, password) {
+    this.identification = identification;
     this.name = name;
     this.lastName = lastName;
+    this.address = address;
+    this.phone = phone;
+    this.birthdate = birthdate;
     this.email = email;
     this.password = password;
   }
 }
 
-const adminUser = new User("admin", "user", "adminuser@sample.com", "password");
+const adminUser = new User("admin", "user", "000000000", "Always Av.", "3008884422", "01/01/1980", "adminuser@sample.com", "password");
 
-// Function to retrieve the list of users from LocalStorage
 function GetUsersList() {
   let usersListJSON = localStorage.getItem('users');
   if (usersListJSON == null) {
@@ -23,46 +26,40 @@ function GetUsersList() {
   return JSON.parse(usersListJSON);
 }
 
-// Function to save the list of users to LocalStorage
 function saveUsersList(usersList) {
   localStorage.setItem('users', JSON.stringify(usersList));
 }
 
-// Function to register a new user
-function RegisterUser(name, lastName, email, password) {
-  // Retrieve the current list of users
+function RegisterUser(name, lastName, identification, address, phone, birthdate, email, password) {
   const usersList = GetUsersList();
 
-  // Check if the email is already in use
-  const isEmailTaken = usersList.some(user => user.email === email);
+  const isEmailOrIdentificationTaken = usersList.some(user => user.email === email || user.identification === identification);
 
-  if (isEmailTaken) {
-    return false; // Email already in use, registration failed
+  if (isEmailOrIdentificationTaken) {
+    return false;
   }
 
-  // Create a new user object
   const newUser = {
+    identification,
     name,
     lastName,
+    address,
+    phone,
+    birthdate,
     email,
     password
   };
 
-  // Add the new user to the list
   usersList.push(newUser);
 
-  // Save the updated list to LocalStorage
   saveUsersList(usersList);
 
-  return true; // Registration successful
+  return true;
 }
 
-// Function to validate user credentials
 function ValidateUser(email, password) {
-  // Retrieve the current list of users
   const usersList = GetUsersList();
 
-  // Check if there is a user with the given email and password
   const isValidUser = usersList.some(user => user.email === email && user.password === password);
 
   if (isValidUser) localStorage.setItem('isUserLogged', JSON.stringify(true));
@@ -75,7 +72,6 @@ function IsUserLogged() {
   return JSON.parse(localStorage.getItem('isUserLogged')) ?? false;
 }
 
-// Export the functions
 export {
   GetUsersList,
   RegisterUser,
